776ff4daa49a2a9f9790a880d5503f167c2ad442
aymenwacheh <aymenwacheh@gmail.com>
2026-07-01 14:10:13 +0200

    correcion backend chabot

--- routes/duree.js
+++ routes/duree.js
@@ -1,17 +1,12 @@
-// nafahat_api/routes/formations.js
+// nafahat_api/routes/duree.js
 const express = require('express');
 const router = express.Router();
-const formationController = require('../controllers/formationController');
+const dureeController = require('../controllers/dureeController');
 
-router.get('/', formationController.getAllFormations);
-router.get('/active', formationController.getActiveFormations);
-router.get('/promos', formationController.getFormationsWithDiscount);
-router.get('/categorie/:id', formationController.getFormationsByCategorie);
-router.get('/admin/all', formationController.getAllFormationsAdmin);
-router.get('/:id', formationController.getFormationById);
-router.post('/', formationController.createFormation);
-router.put('/:id', formationController.updateFormation);
-router.delete('/:id', formationController.deleteFormation);
-router.delete('/hard/:id', formationController.hardDeleteFormation);
+router.get('/', dureeController.getAllDuree);
+router.get('/:id', dureeController.getDureeById);
+router.post('/', dureeController.createDuree);
+router.put('/:id', dureeController.updateDuree);
+router.delete('/:id', dureeController.deleteDuree);
 
 module.exports = router;
